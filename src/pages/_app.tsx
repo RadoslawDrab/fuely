c093e88166c2c80102a3919e861ce7a472e1a293
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-26 15:43:23 +0200

    implemented AppContextWrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@styles/globals.scss'
 import type { AppProps } from 'next/app'
+import AppContextWrapper from '@/utils/AppContextWrapper'
+import '@styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<AppContextWrapper>
+			<Component {...pageProps} />
+		</AppContextWrapper>
+	)
 }
