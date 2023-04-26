1ecf9a0f676634b479f841766d55331e56294ac7
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-26 15:44:57 +0200

    added Layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+// import styles from './Layout.module.scss';
+
+interface Props {
+	children?: any
+}
+function Layout(props: Props) {
+	return (
+		<div>
+			<Header />
+			<main>{props.children}</main>
+			<Footer />
+		</div>
+	)
+}
+
+export default Layout
