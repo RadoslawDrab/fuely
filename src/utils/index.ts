ecc0b702e8888aa68bc3b297dcdbe884b0b93ea9
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-26 15:45:27 +0200

    added random key generator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,12 @@
 export function isClient() {
 	return typeof window !== 'undefined'
 }
+export function getRandomKey(digitsCount: number = 6): string {
+	const digits = []
+	for (let i = 0; i < Math.max(digitsCount, 1); i++) {
+		const randomKey = Math.round(Math.random() * 9)
+		digits.push(randomKey)
+	}
+	const str = digits.reduce((acc, val) => (acc += val), '')
+	return str
+}
