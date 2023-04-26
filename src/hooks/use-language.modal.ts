b994d661bc4cb381b51b79d43fe445023941aa31
Radosław Drab <radoslaw.drab03@gmail.com>
2023-04-26 15:40:59 +0200

    created language hook

---
+++ src/hooks/use-language.modal.ts
@@ -0,0 +1,23 @@
+import translations from '@data/translations.json'
+
+// Available texts
+export type Texts = keyof typeof translations
+
+// Languages other than English
+type OtherLanguages = keyof (typeof translations)['Theme']
+// All languages
+export type Languages = 'en' | OtherLanguages
+
+// Translation object type
+export type Translation = { [key in OtherLanguages | string]: string }
+// All available translations type
+export type Translations = {
+	[key in Texts]: Translation
+}
+
+export type LanguageObject = {
+	getText: (text: Texts) => string
+	setLanguage: (lang: Languages) => void
+	language: Languages | string
+	languages: Languages[]
+}
---
+++ src/hooks/use-language.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import translations from '@data/translations.json'
+
+import { LanguageObject, Languages, Texts, Translations } from './use-language.modal'
+
+const translationsObject: Translations = translations
+
+const allLanguages: any = ['en', ...Object.keys(translations.Button)]
+function useLanguage(): LanguageObject {
+	const [currentLang, setCurrentLanguage] = useState<Languages | string>('en')
+
+	useEffect(() => {
+		// Changes language based on language set in browser
+		setCurrentLanguage(() => navigator.language.split('-')[0])
+	}, [])
+	// Sets language
+	function setLanguage(lang: Languages) {
+		setCurrentLanguage(() => lang)
+	}
+	function getText(text: Texts): string {
+		// Returns current text with English language is selected
+		if (currentLang === 'en') return text
+
+		// Translation found for `text`
+		const foundTranslation = translationsObject[text]
+		// Translation for current language
+		return foundTranslation[currentLang] || text
+	}
+	const languages: Languages[] = allLanguages
+	return { getText, setLanguage, language: currentLang, languages }
+}
+
+export default useLanguage
+
+export const exampleLanguageObject: LanguageObject = {
+	setLanguage: () => {},
+	getText: () => '',
+	language: allLanguages[0],
+	languages: [...allLanguages]
+}
