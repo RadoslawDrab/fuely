3d77c2c8fed2ce7ac2f7e5a186bbf7babc51fd67
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-06 17:40:18 +0200

    changed from index to Button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './Button.module.scss'
+import styles from '@comp-styles/UI/Button.module.scss'
 
 export default function Button(props: any) {
 	return <button></button>
