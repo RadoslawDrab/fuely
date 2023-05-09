20e4a542ff6190cce0362839d53a1ba63fdb66e2
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-09 14:16:28 +0200

    added new icons

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,2 +1,14 @@
-const icons = ['logo', 'equal'] as const
+const icons = [
+	'logo',
+	'equal',
+	'house',
+	'gearshape',
+	'fuelpump',
+	'arrow.right',
+	'arrow.up.right.square',
+	'rectangle.3.group',
+	'person',
+	'person.crop.circle.badge.plus',
+	'person.crop.circle'
+] as const
 export default icons
