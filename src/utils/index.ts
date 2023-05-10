0e9fd1bbc00fe7e32376ab1080b39578a22d3b42
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-10 16:59:07 +0200

    added exception to random key generator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,13 +3,17 @@ import { Languages } from '@/hooks/Language.modal'
 export function isClient() {
 	return typeof window !== 'undefined'
 }
-export function getRandomKey(digitsCount: number = 6): string {
-	const digits = []
-	for (let i = 0; i < Math.max(digitsCount, 1); i++) {
-		const randomKey = Math.round(Math.random() * 9)
-		digits.push(randomKey)
-	}
-	const str = digits.reduce((acc, val) => (acc += val), '')
+export function getRandomKey(digitsCount: number = 6, exceptions: string[] = []): string {
+	let str = ''
+	do {
+		const digits = []
+		for (let i = 0; i < Math.max(digitsCount, 1); i++) {
+			const randomKey = Math.round(Math.random() * 9)
+			digits.push(randomKey)
+		}
+		str = digits.reduce((acc, val) => (acc += val), '')
+	} while (exceptions.find((exception) => exception === str))
+
 	return str
 }
 
