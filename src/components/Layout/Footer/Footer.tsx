179dd6b6cf096e78ba0018d9761bbc35432a2f2b
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-12 23:19:34 +0200

    added basic footer content

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
-// import styles from './Footer.module.scss';
+
+import useAppContext from '@/hooks/use-app-context'
+import { getRandomKey } from '@/utils'
+
+import Button from '@/components/UI/Button'
+
+import styles from '@styles/Layout/Footer.module.scss'
 
 function Footer() {
-	return <footer></footer>
+	const { setLanguage, languages, getText } = useAppContext().Language
+	const { toggleTheme } = useAppContext().Theme
+
+	function onLanguageChange(e: React.MouseEvent<HTMLButtonElement>) {
+		const buttonLang: any = e.currentTarget.dataset.language
+		setLanguage(buttonLang || 'en')
+	}
+	const languageButtons = languages.map((lang) => {
+		const language = lang.toString()
+		const key = `${language}-${getRandomKey()}`
+		return (
+			<li key={key}>
+				<Button onClick={onLanguageChange} data={{ language: language }}>
+					{language}
+				</Button>
+			</li>
+		)
+	})
+	return (
+		<footer className={styles.footer}>
+			<ul>{languageButtons}</ul>
+			<Button onClick={toggleTheme}>{getText('Theme')}</Button>
+		</footer>
+	)
 }
 
 export default Footer
