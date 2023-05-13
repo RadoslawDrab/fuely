d4e17cbd0292acb03995e1a3a1bb99f2adf2cb72
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-13 14:31:02 +0200

    moved array and added condition checking

--- src/components/Layout/Header/Navigation.tsx
+++ src/components/Layout/Header/Navigation.tsx
@@ -12,51 +12,62 @@ import Icon from '@/components/UI/Icon'
 
 import styles from '@styles/Layout/Header/Navigation.module.scss'
 
-const navButtons: NavigationButton[] = [
-	{
-		name: 'Home',
-		icon: 'house',
-		path: '/'
-	},
-	{
-		name: 'Dashboard',
-		icon: 'rectangle.3.group',
-		path: '/user/[userId]'
-	},
-	{
-		name: 'Login',
-		icon: 'person.crop.circle',
-		path: '/user/login'
-	},
-	{
-		name: 'Register',
-		icon: 'person.crop.circle.badge.plus',
-		path: '/user/register'
-	},
-	{
-		name: 'Account',
-		icon: 'person',
-		path: '/user/account'
-	},
-	{
-		name: 'Settings',
-		icon: 'gearshape',
-		path: '/settings'
-	}
-]
 export default function Navigation() {
 	const router = useRouter()
 
 	const { navigationState } = useAppContext().Navigation
+	const { isLoggedIn } = useAppContext().Auth
+
+	const navButtons: NavigationButton[] = [
+		{
+			name: 'Home',
+			icon: 'house',
+			path: '/'
+		},
+		{
+			name: 'Dashboard',
+			icon: 'rectangle.3.group',
+			path: '/user/[userId]',
+			condition: () => isLoggedIn
+		},
+		{
+			name: 'Login',
+			icon: 'person.crop.circle',
+			path: '/user/login',
+			condition: () => !isLoggedIn
+		},
+		{
+			name: 'Register',
+			icon: 'person.crop.circle.badge.plus',
+			path: '/user/register',
+			condition: () => !isLoggedIn
+		},
+		{
+			name: 'Account',
+			icon: 'person',
+			path: '/user/account',
+			condition: () => isLoggedIn
+		},
+		{
+			name: 'Settings',
+			icon: 'gearshape',
+			path: '/settings'
+		}
+	]
 
 	const navigationStyles = className(styles.nav, !navigationState ? 'hidden' : '')
 
+	// Changes url based on path
 	function navigate(event: React.MouseEvent<HTMLButtonElement>) {
 		const url = event.currentTarget.dataset.path ?? '/'
 		router.replace(url)
 	}
 
 	const navigationButtons = navButtons.map((item, i) => {
+		// Checks if condition is met
+		if (item.condition && !item.condition()) {
+			return
+		}
 		return (
 			<li key={i}>
 				<Button onClick={navigate} data={{ path: item.path }}>
