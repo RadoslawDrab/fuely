d52abf02f4fd3ec7d2f7e50a23fcdc7d4b25f5bf
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-15 17:05:48 +0200

    added firebase config

---
+++ src/pages/api/auth/_firebase.ts
@@ -0,0 +1,16 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+const firebaseConfig: FirebaseOptions = {
+	apiKey: 'AIzaSyAtg8qAfoguxzSkHXIEzg4kTF-g7w4y4G4',
+	authDomain: 'fuely-app.firebaseapp.com',
+	databaseURL: 'https://fuely-app-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'fuely-app',
+	storageBucket: 'fuely-app.appspot.com',
+	messagingSenderId: '202397902963',
+	appId: '1:202397902963:web:48fef5398d485c1130e206',
+	measurementId: 'G-CWXDPY7FZZ'
+}
+
+const app = initializeApp(firebaseConfig)
+export default getDatabase(app)
