8ef484b5b92b63d119718f5213b893e0ce1b9af4
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-15 13:49:25 +0200

    implemented simple login

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,6 +1,44 @@
-import Layout from '@/components/Layout/Layout'
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Login(props: any) {
-	return <Layout>Login</Layout>
+import useAppContext from '@/hooks/use-app-context'
+import useUserRedirect from '@/hooks/use-user-redirect'
+
+import Layout from '@/components/Layout/Layout'
+import Button from '@/components/UI/Button'
+
+export default function Login() {
+	const { login, loginUsingToken } = useAppContext().Auth
+	const [inputLogin, setInputLogin] = useState('Radek')
+	const [inputPassword, setInputPassword] = useState('Password')
+	const [hasError, setHasError] = useState(false)
+
+	useUserRedirect()
+
+	function loginUser() {
+		login(inputLogin, inputPassword)
+			.then((token) => {
+				loginUsingToken(token)
+				setHasError(() => false)
+			})
+			.catch((error) => {
+				console.error(error)
+
+				setHasError(() => true)
+			})
+	}
+	function onLoginInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+		setInputLogin(() => event.target.value)
+	}
+	function onPasswordInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+		setInputPassword(() => event.target.value)
+	}
+
+	return (
+		<Layout>
+			<Button onClick={loginUser}>Login</Button>
+			<input type="text" defaultValue={inputLogin} onChange={onLoginInputChange} />
+			<input type="text" defaultValue={inputPassword} onChange={onPasswordInputChange} />
+			{hasError && <p>Invalid login or password</p>}
+		</Layout>
+	)
 }
