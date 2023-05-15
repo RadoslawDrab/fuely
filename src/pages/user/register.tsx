8d6a51005e30fdb752fff0c29d64cba553769719
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-15 13:49:37 +0200

    implemented simple register

--- src/pages/user/register.tsx
+++ src/pages/user/register.tsx
@@ -1,6 +1,30 @@
-import Layout from '@/components/Layout/Layout'
 import React from 'react'
+import { useRouter } from 'next/router'
 
-export default function Register(props: any) {
-	return <Layout>Register</Layout>
+import useAppContext from '@/hooks/use-app-context'
+import useUserRedirect from '@/hooks/use-user-redirect'
+
+import Layout from '@/components/Layout/Layout'
+import Button from '@/components/UI/Button'
+
+export default function Register() {
+	const router = useRouter()
+	const { register } = useAppContext().Auth
+
+	useUserRedirect()
+
+	function registerUser() {
+		register('Radek', 'Password')
+			.then(() => {
+				router.replace('/user/login')
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
+	return (
+		<Layout>
+			<Button onClick={registerUser}>Register</Button>
+		</Layout>
+	)
 }
