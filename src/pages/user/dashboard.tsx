278e3454bd259e49655e422361ff38044b397e81
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-16 19:15:32 +0200

    removed unnecessary code

--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -1,19 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import useAppContext from '@/hooks/use-app-context'
 
 export default function Dashboard() {
 	const { user, isLoading } = useAppContext().Auth
-	const { setTheme } = useAppContext().Theme
-	const { setLanguage } = useAppContext().Language
-
-	useEffect(() => {
-		if (user) {
-			setLanguage(user.settings.language)
-			setTheme(user.settings.theme === 'dark')
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user])
 
 	if (isLoading) {
 		return <>Loading...</>
