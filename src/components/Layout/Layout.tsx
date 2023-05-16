f60c2de2798dbb8cf834a5776ea863a5e3884cdd
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-16 19:15:11 +0200

    moved setting theme and language based on user

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import useAppContext from '@/hooks/use-app-context'
 
 import Header from './Header'
 import Footer from './Footer/Footer'
@@ -8,6 +10,18 @@ interface Props {
 	children?: any
 }
 function Layout(props: Props) {
+	const { user } = useAppContext().Auth
+	const { setTheme } = useAppContext().Theme
+	const { setLanguage } = useAppContext().Language
+
+	useEffect(() => {
+		if (user) {
+			setLanguage(user.settings.language)
+			setTheme(user.settings.theme === 'dark')
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [user])
+
 	return (
 		<div>
 			<Header />
