5ce5fc641bc441c7ed3cbc7e82b453f44a6fc397
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-16 19:15:49 +0200

    added basic styling

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,8 @@ import useAppContext from '@/hooks/use-app-context'
 
 import Header from './Header'
 import Footer from './Footer/Footer'
-// import styles from './Layout.module.scss';
+
+import styles from '@styles/Layout/Layout.module.scss'
 
 interface Props {
 	children?: any
@@ -23,7 +24,7 @@ function Layout(props: Props) {
 	}, [user])
 
 	return (
-		<div>
+		<div className={styles.layout}>
 			<Header />
 			<main>{props.children}</main>
 			<Footer />
