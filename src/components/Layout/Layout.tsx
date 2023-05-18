e29323f39166440820ebd3d3ca5c7580fd6e5551
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-18 14:47:00 +0200

    moved Footer component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Header from './Header'
-import Footer from './Footer/Footer'
+import Footer from './Footer'
 
 import styles from '@styles/Layout/Layout.module.scss'
 
