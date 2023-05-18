25f159d06c46c3fe38d385cb395a97863ec62edd
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-18 14:47:36 +0200

    added generic input element

---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Props } from '@/types/UI/Input.modal'
+import { className } from '@/utils'
+
+import styles from '@styles/UI/Input.module.scss'
+
+export default function Input(props: Props) {
+	const inputStyles = className(styles.input, props.className)
+	return (
+		<input
+			id={props.id}
+			className={inputStyles}
+			type={props.type}
+			placeholder={props.placeholder}
+			defaultValue={props.defaultValue}
+			onChange={props.onChange}
+			onFocus={props.onFocus}
+			onBlur={props.onBlur}
+			disabled={props.disabled}
+			max={props.max}
+			maxLength={props.max}
+			min={props.min}
+			minLength={props.min}
+		/>
+	)
+}
---
+++ src/types/UI/Input.modal.ts
@@ -0,0 +1,15 @@
+import { ChangeEvent, FocusEvent, HTMLInputTypeAttribute } from 'react'
+
+export interface Props {
+	type: HTMLInputTypeAttribute
+	className?: string
+	placeholder?: string
+	defaultValue?: string
+	id?: string
+	disabled?: boolean
+	min?: number
+	max?: number
+	onChange: (event: ChangeEvent<HTMLInputElement>) => void
+	onBlur?: (event: FocusEvent<HTMLInputElement>) => void
+	onFocus?: (event: FocusEvent<HTMLInputElement>) => void
+}
