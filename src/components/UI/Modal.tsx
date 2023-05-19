d73ac7e6905485e464186048a97a6ec4d8d5a4a7
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-19 11:16:16 +0200

    added Modal window component

---
+++ src/components/UI/Modal.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Button from './Button'
+import styles from '@styles/UI/Modal.module.scss'
+import { className } from '@/utils'
+import Icon from './Icon'
+
+interface Props {
+	children?: any
+	show: boolean
+	allowClosing?: boolean
+	className?: string
+	type?: 'center' | 'flow'
+	title: string
+	state?: (isOpen: boolean) => void
+}
+export default function Modal(props: Props) {
+	const modalType = props.type ?? 'flow'
+	const modalStyles = className(styles.modal, styles[`modal__${modalType}`], props.className)
+
+	if (!props.show) return <></>
+
+	return (
+		<dialog open={props.show} className={modalStyles}>
+			<div className={styles['modal-bar']}>
+				<span>{props.title}</span>
+				{props.allowClosing && (
+					<form method="dialog">
+						<Button className="close-button" onClick={() => {}}>
+							<Icon type="xmark" alt="close icon" />
+						</Button>
+					</form>
+				)}
+			</div>
+			<div className={styles['modal-content']}>{props.children}</div>
+		</dialog>
+	)
+}
