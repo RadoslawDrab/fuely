6f96b6e196b7c16c721edeb4288688bc7618f229
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-19 11:15:22 +0200

    added Error element

---
+++ src/components/UI/Error.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { className } from '@/utils'
+
+import styles from '@styles/UI/Error.module.scss'
+
+interface Props {
+	show: boolean
+	text: string
+	className?: string
+}
+export default function Error(props: Props) {
+	const errorStyles = className(styles.error, props.className)
+	return <>{props.show && <span className={errorStyles}>{props.text}</span>}</>
+}
