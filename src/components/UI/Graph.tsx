35e5dfe7cfb49254b66dc385d2e2bdceb9f9563b
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-23 23:09:17 +0200

    added Graph component

---
+++ src/components/UI/Graph.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { className, getRandomKey } from '@/utils'
+
+import styles from '@styles/UI/Graph.module.scss'
+
+interface Props {
+	min: number
+	items: { name?: string; value: number }[]
+	max?: number
+	meter?: boolean
+	className?: string
+}
+export default function Graph(props: Props) {
+	const graphStyles = className(styles.graph, props.meter ? styles.meter : '', props.className)
+
+	const progressElements = props.items.map((item, i) => {
+		const key = `${i}-${getRandomKey()}`
+		const labelId = `progress-bar-${i}`
+
+		return (
+			<div key={key} className={styles.bar}>
+				<progress id={labelId} max={props.max} value={item.value}>
+					at {item.value}/{props.max}
+				</progress>
+				{item.name && <label htmlFor={labelId}>{item.name}</label>}
+			</div>
+		)
+	})
+	return <div className={graphStyles}>{progressElements}</div>
+}
