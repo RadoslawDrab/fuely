b8b65a8192519595348217e0e2a11141543ce087
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-23 22:05:09 +0200

    added section styling and more props

--- src/components/Layout/Section.tsx
+++ src/components/Layout/Section.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
+import { className } from '@/utils'
+
 import styles from '@styles/Layout/Section.module.scss'
 
 interface Props {
-	children?: any
 	title: string
+	children?: any
+	className?: string
+	contentClassName?: string
 }
 export default function Section(props: Props) {
+	const sectionStyles = className(styles.section, props.className)
+	const sectionContentStyles = className(styles.content, props.contentClassName)
 	return (
-		<section className={styles.section}>
+		<section className={sectionStyles}>
 			<h2>{props.title}</h2>
-			<div className={styles['section-content']}>{props.children}</div>
+			<div className={sectionContentStyles}>{props.children}</div>
 		</section>
 	)
 }
