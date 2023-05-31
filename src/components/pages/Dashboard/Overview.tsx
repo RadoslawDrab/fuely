b6443a39ebb623a49a4c8bf7fd9ff0a26b31d32b
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-31 14:20:14 +0200

    moved Overview content

--- src/components/pages/Dashboard/Overview.tsx
+++ src/components/pages/Dashboard/Overview.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import { className, getRandomKey } from '@/utils'
+import useEvents from '@/hooks/use-events'
+import useUnit from '@/hooks/use-unit'
 
 import Section from '@/components/Layout/Section'
 import Graph from '@/components/UI/Graph'
@@ -10,22 +12,67 @@ import defaultStyles from '@styles/styles.module.scss'
 
 interface Props {
 	className?: string
-	date: string
-	items: {
-		label: string
-		currentValue: number
-		previousValue: number
-		unit: string | JSX.Element
-		digits?: number
-	}[]
 }
+interface Item {
+	label: string
+	currentValue: number
+	previousValue: number
+	unit: string | JSX.Element
+	digits?: number
+}
+
 export default function Overview(props: Props) {
+	const { getEvent, isLoading } = useEvents()
+	const { isMetric, units } = useUnit()
+
+	if (isLoading) return <></>
+
+	// Last event
+	const event0 = getEvent(0)
+	// Second to last event
+	const event1 = getEvent(1)
+
+	if (!event0 || !event1) return <></>
+
+	// Styling
 	const sectionStyles = className(defaultStyles.section, props.className)
 
-	const itemElements = props.items.map((item, i) => {
+	// Last consumption
+	const currentConsumption = isMetric ? (event0.fuel / event0.distance) * 100 : event0.distance / event0.fuel
+	// Second to last consumption
+	const previousConsumption = isMetric ? (event1.fuel / event1.distance) * 100 : event1.distance / event1.fuel
+
+	const items: Item[] = [
+		{ label: 'Prize', currentValue: event0.cost, previousValue: event1.cost, unit: units.currency },
+		{ label: 'Fuel', currentValue: event0.fuel, previousValue: event1.fuel, unit: units.fuel },
+		{
+			label: 'Distance',
+			currentValue: event0.distance,
+			previousValue: event1.distance,
+			unit: units.distance,
+			digits: 0
+		},
+		{
+			label: 'Consumption',
+			currentValue: currentConsumption,
+			previousValue: previousConsumption,
+			unit: isMetric ? (
+				<>
+					<sup>{units.fuel}</sup>/<sub>100{units.distance}</sub>
+				</>
+			) : (
+				<>
+					<sup>{units.distance}</sup>/<sub>{units.fuel}</sub>
+				</>
+			),
+			digits: 2
+		}
+	]
+	const itemElements = items.map((item, i) => {
 		const key = `${i}-${getRandomKey()}`
 
 		const percent = item.currentValue / item.previousValue - 1
+		// Adds + or - based on percent
 		const percentString = `${percent > 0 ? '+' : '-'}${Math.abs(percent * 100).toFixed(1)}`
 
 		const value = item.currentValue.toFixed(item.digits ?? 1)
@@ -57,7 +104,7 @@ export default function Overview(props: Props) {
 		<Section title="Overview" className={sectionStyles} contentClassName={styles.section}>
 			<header>
 				<h3>Last refuel</h3>
-				<time dateTime={props.date}>{props.date}</time>
+				<time dateTime={event0.date}>{event0.date}</time>
 			</header>
 			<hr />
 			<div className={styles.main}>{itemElements}</div>
--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import useAppContext from '@/hooks/use-app-context'
 import useUserRedirect from '@/hooks/use-user-redirect'
-import { getUnit } from '@/utils'
 import useEvents from '@/hooks/use-events'
+import useUnit from '@/hooks/use-unit'
 
 import Overview from '@/components/pages/Dashboard/Overview'
 import DashboardComponent from '@/components/pages/Dashboard/Dashboard'
@@ -15,6 +15,7 @@ import styles from '@styles/pages/Dashboard/index.module.scss'
 export default function Dashboard() {
 	const { user, isLoading } = useAppContext().Auth
 	const { events, sortedDates, formatDate } = useEvents()
+	const { units } = useUnit()
 
 	useUserRedirect()
 
@@ -27,35 +28,16 @@ export default function Dashboard() {
 		return {
 			id: date,
 			cost: event.cost,
-			costUnit: user.currency,
+			costUnit: units.currency,
 			fuel: event.fuel,
-			fuelUnit: getUnit(user.fuelUnit),
+			fuelUnit: units.fuel,
 			date: formatDate(date)
 		}
 	})
 
 	return (
 		<>
-			<Overview
-				className={styles.overview}
-				date="23 May 2023"
-				items={[
-					{ label: 'Prize', currentValue: 250, previousValue: 230, unit: 'zł' },
-					{ label: 'Fuel', currentValue: 35, previousValue: 30, unit: 'L' },
-					{ label: 'Distance', currentValue: 412, previousValue: 389, unit: 'km', digits: 0 },
-					{
-						label: 'Consumption',
-						currentValue: 8.38,
-						previousValue: 8.87,
-						unit: (
-							<>
-								<sup>L</sup>/<sub>100km</sub>
-							</>
-						),
-						digits: 2
-					}
-				]}
-			/>
+			<Overview className={styles.overview} />
 			<Button className={styles['new-refuel-button']} onClick={() => {}}>
 				<Icon type="fuelpump" alt="fuelpump icon" />
 				<span>New Refuel</span>
