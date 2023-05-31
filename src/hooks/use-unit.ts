8aed144f9ef3ce9c5d97a5f60a48a2fe55cb36dd
Radosław Drab <radoslaw.drab03@gmail.com>
2023-05-31 16:54:35 +0200

    added conversion

--- src/hooks/use-unit.ts
+++ src/hooks/use-unit.ts
@@ -1,8 +1,15 @@
 import useAppContext from './use-app-context'
 
+export type UnitType = 'distance' | 'fuel'
 export default function useUnit() {
 	const { user } = useAppContext().Auth
 
+	const conversion = {
+		// kilometers > miles
+		distance: 0.621371192,
+		// liters > galons
+		fuel: 0.264172052
+	}
 	const units = {
 		distance: 'km',
 		fuel: 'L',
@@ -20,5 +27,7 @@ export default function useUnit() {
 	return {
 		system: user.settings?.units ?? 'metric',
 		isMetric: (user.settings?.units ?? 'metric') === 'metric',
+		units,
+		conversion
 	}
 }
