8946acbe59ed447ba8f1100b48eebce26b1ba209
Radosław Drab <radoslaw.drab03@gmail.com>
2023-06-02 16:47:59 +0200

    added car loading component

--- src/components/UI/LoadingIcon.tsx
+++ src/components/UI/LoadingIcon.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-import styles from '@styles/Layout/UI/LoadingIcon.module.scss'
+import styles from '@styles/UI/LoadingIcon.module.scss'
+import Icon from './Icon'
 
-export default function LoadingIcon(props: any) {
-	return <div>Loading...</div>
+export default function LoadingIcon() {
+	return (
+		<div className={styles.loading}>
+			<div className={styles.car}>
+				<Icon type="car" alt="car icon" />
+			</div>
+		</div>
+	)
 }
