d70ce0612e9557bc119439890a204d7eda3e631c
Radosław Drab <radoslaw.drab03@gmail.com>
2023-06-07 13:02:25 +0200

    remove formData on reload

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,10 +4,12 @@ import Header from './Header'
 import Footer from './Footer'
 
 import styles from '@styles/Layout/Layout.module.scss'
+import { setLocalStorage } from '@/utils'
 
 interface Props {
 	children?: any
 }
+setLocalStorage({ formData: undefined })
 function Layout(props: Props) {
 	return (
 		<div className={styles.layout}>
