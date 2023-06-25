c3b1aed763f73f1008e4016558a9c8284de5b80d
Radosław Drab <radoslaw.drab03@gmail.com>
2023-06-25 18:13:47 +0200

    added centering prop

--- src/components/UI/LoadingIcon.tsx
+++ src/components/UI/LoadingIcon.tsx
@@ -1,16 +1,23 @@
 import Head from 'next/head'
 import React from 'react'
 
-import styles from '@styles/UI/LoadingIcon.module.scss'
+import { className } from '@/utils'
+
 import Icon from './Icon'
 
-export default function LoadingIcon() {
+import styles from '@styles/UI/LoadingIcon.module.scss'
+
+interface Props {
+	center?: boolean
+}
+export default function LoadingIcon(props: Props) {
+	const loadingIconStyles = className(styles.loading, 'loading-icon', props.center ? styles.center : '')
 	return (
 		<>
 			<Head>
 				<title>Fuely | Loading</title>
 			</Head>
-			<div className={styles.loading} role="status">
+			<div className={loadingIconStyles} role="status">
 				<div className={styles.car}>
 					<Icon type="car" alt="car icon" />
 				</div>
