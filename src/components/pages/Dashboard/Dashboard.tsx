c38c3b60e0a342a645846adff5f87965729c0fec
Radosław Drab <radoslaw.drab03@gmail.com>
2023-07-10 23:59:01 +0200

    added filter and page controls

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -2,9 +2,9 @@ import { useRouter } from 'next/router'
 import React, { useEffect, useMemo, useState } from 'react'
 
 import useEvents from '@/hooks/Events/use-events'
+import useAppContext from '@/hooks/Other/use-app-context'
 import useUnit from '@/hooks/Other/use-unit'
 import { className, getRandomKey } from '@/utils'
-import useAppContext from '@/hooks/Other/use-app-context'
 
 import { FullEvent } from '@/hooks/Events/types/Events.modal'
 import { DashboardItem, DashboardProps as Props } from './types/Dashboard.modal'
@@ -12,10 +12,15 @@ import { DashboardItem, DashboardProps as Props } from './types/Dashboard.modal'
 import Section from '@/components/Layout/Section'
 import Button from '@/components/UI/Button'
 import Icon from '@/components/UI/Icon'
+import LoadingIcon from '@/components/UI/LoadingIcon'
+import Select from '@/components/UI/Select'
 
 import styles from '@styles/pages/Dashboard/Dashboard.module.scss'
 import defaultStyles from '@styles/styles.module.scss'
 
+const maxSkip = 5
+const perPageValues = [3, 5, 10]
+const perPageOptions = perPageValues.map((value) => ({ value: value.toString() }))
 export default function Dashboard(props: Props) {
 	const router = useRouter()
 
@@ -25,12 +30,24 @@ export default function Dashboard(props: Props) {
 	const { units } = useUnit()
 
 	const [dashboardEvents, setDashboardEvents] = useState<FullEvent[]>([])
+	const [currentPage, setCurrentPage] = useState<number>(0)
+	const [perPage, setPerPage] = useState<number>(perPageValues[0])
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+
+	const pagesCount = Math.ceil(sortedDates.length / perPage)
 
 	useEffect(() => {
-		Promise.all(sortedDates.map((_, i) => getEvent(i))).then((events) => {
-			setDashboardEvents(() => events)
-		})
-	}, [getEvent, sortedDates])
+		const dates = sortedDates.slice(currentPage * perPage, currentPage * perPage + perPage)
+
+		setIsLoading(true)
+		Promise.all(dates.map((_, i) => getEvent(i, dates)))
+			.then((events) => {
+				setDashboardEvents(events)
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
+	}, [currentPage, getEvent, perPage, sortedDates])
 
 	const dashboardItems: DashboardItem[] = useMemo(() => {
 		return dashboardEvents.map((event) => {
@@ -51,6 +68,13 @@ export default function Dashboard(props: Props) {
 		const path = `/${event.currentTarget.dataset.id}` || '/dashboard'
 
 		router.push(path)
+	}
+	function clamp(value: number): number {
+		return Math.max(Math.min(value, pagesCount - 1), 0)
+	}
+	function changeCurrentPage(event: React.MouseEvent<HTMLButtonElement>) {
+		const pageChangeAmount = +(event.currentTarget.dataset.amount || 1)
+		setCurrentPage((page) => clamp(page + pageChangeAmount))
 	}
 
 	const rows = dashboardItems.map((item, i) => {
@@ -79,8 +103,28 @@ export default function Dashboard(props: Props) {
 	})
 	return (
 		<Section title={getText('Dashboard')} className={props.className} contentClassName={sectionStyles}>
-			{rows.length > 0 && <ul>{rows}</ul>}
-			{rows.length <= 0 && <span>No events</span>}
+			<div className={styles.filter}>
+				<label htmlFor="per-page-select">Show</label>
+				<Select id="per-page-select" options={perPageOptions} getValue={(value) => setPerPage(+value)} />
+			</div>
+			{isLoading && <LoadingIcon />}
+			{!isLoading && rows.length > 0 && <ul style={{ '--per-page': perPage } as React.CSSProperties}>{rows}</ul>}
+			{!isLoading && rows.length <= 0 && <span>No events</span>}
+			<div className={styles.controls}>
+				<Button onClick={changeCurrentPage} data={{ amount: -maxSkip }} variant="redirect">
+					<Icon type="caret-double-left" alt="previous page icon" />
+				</Button>
+				<Button onClick={changeCurrentPage} data={{ amount: -1 }} variant="redirect">
+					<Icon type="caret-left" alt="previous page icon" />
+				</Button>
+				<span>{currentPage + 1}</span>
+				<Button onClick={changeCurrentPage} data={{ amount: 1 }} variant="redirect">
+					<Icon type="caret-right" alt="next page icon" />
+				</Button>
+				<Button onClick={changeCurrentPage} data={{ amount: maxSkip }} variant="redirect">
+					<Icon type="caret-double-right" alt="next page icon" />
+				</Button>
+			</div>
 		</Section>
 	)
 }
