653c2453744bead8eb3d0f68af3e604468bf21b9
Radosław Drab <radoslaw.drab03@gmail.com>
2023-07-16 21:12:59 +0200

    modified item displaying style

--- src/components/pages/Item/InfoItem.tsx
+++ src/components/pages/Item/InfoItem.tsx
@@ -4,31 +4,32 @@ import { className } from '@/utils'
 
 import { InfoItemProps as Props } from './types/Info.modal'
 
+import Graph from '@/components/UI/Graph'
+
 import styles from '@styles/pages/Item/InfoItem.module.scss'
 
 export default function InfoItem(props: Props) {
-	const dateItems = props.items.map((item, index) => {
-		const key = `${item.date + '-' + index}`
+	const graphItems = props.items.map((item) => {
 		const percentString = `${item.percent === 0 ? '' : item.percent >= 0 ? '+' : '-'}${Math.abs(item.percent).toFixed(2)}%`
-
 		const currentClassName = item.isCurrent ? styles.current : ''
-		const dateClassName = className(styles.date, currentClassName)
-		const valueClassName = className(styles.value, currentClassName)
-		const percentClassName = className(styles.percent, currentClassName)
 
-		return (
-			<React.Fragment key={key}>
-				<time className={dateClassName} dateTime={item.date}>
-					{item.date}
-				</time>
-				<data className={valueClassName} value={item.value}>
-					{item.value} {item.isCurrent && <span>{item.unit}</span>}
-				</data>
-				<data className={percentClassName} value={item.percent}>
-					{percentString}
-				</data>
-			</React.Fragment>
-		)
+		return {
+			name: (
+				<>
+					<span className={currentClassName}>{item.date}</span>
+					<span className={currentClassName}>
+						{!item.isCurrent && <span className={className(currentClassName, styles.percent)}>({percentString})</span>}
+						{item.value} {item.unit}
+					</span>
+				</>
+			),
+			value: +item.value
+		}
 	})
-	return <div className={styles.item}>{dateItems}</div>
+
+	return (
+		<div className={styles.item}>
+			<Graph items={graphItems} alignVertically graphScaling={{ boundary: 0.04, mainValue: props.item.value }} />
+		</div>
+	)
 }
