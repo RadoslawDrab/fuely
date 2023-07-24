7ec379ccc78bc9996c54c59b6bf5e035b6350b24
Radosław Drab <radoslaw.drab03@gmail.com>
2023-07-24 12:28:20 +0200

    fixed bug with 0

--- src/pages/api/user/refuel.ts
+++ src/pages/api/user/refuel.ts
@@ -15,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			case 'POST': {
 				const { date, cost, currency, odometer, fuel } = parseBody(req)
 
-				if (!cost || !odometer || !fuel) {
+				if ((cost !== undefined && cost <= 0) || (odometer !== undefined && odometer < 0) || (fuel !== undefined && fuel <= 0)) {
 					return returnError(res, 'event/not-enough-data')
 				}
 				const { currentUser } = auth
