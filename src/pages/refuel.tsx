38c02120a676024a45dcc259efb56a8227160ccc
Radosław Drab <radoslaw.drab03@gmail.com>
2023-07-24 12:48:04 +0200

    added error handling and loading state

--- src/pages/refuel.tsx
+++ src/pages/refuel.tsx
@@ -3,79 +3,67 @@ import React, { useState } from 'react'
 
 import useAppContext from '@/hooks/Other/use-app-context'
 import useUserRedirect from '@/hooks/Other/use-user-redirect'
-import { className, setSessionStorage } from '@/utils'
-import { Status } from './api/data/types/index.modal'
+import { className } from '@/utils'
+import { getMessage } from '@/utils/messages'
 
-import { Event } from '@/hooks/Events/types/Events.modal'
+import { RefuelFormData } from '@/components/pages/Refuel/types/RefuelForm.modal'
+import { Status } from './api/data/types/index.modal'
 
 import Head from '@/components/Head'
 import Section from '@/components/Layout/Section'
-import Error from '@/components/UI/Error'
 import LoadingIcon from '@/components/UI/LoadingIcon'
 import RefuelForm from '@/components/pages/Refuel/RefuelForm'
 
 import styles from '@styles/styles.module.scss'
 
-const errorTypes: any = {
-	'event/not-enough-data': 'Not enough data provided',
-	'auth/no-user': 'User not logged in'
-}
 export default function Refuel() {
-	useUserRedirect()
 	const router = useRouter()
 
+	useUserRedirect()
 	const { getEvents } = useAppContext().Auth
+	const { addNotification } = useAppContext().Notification
 
 	const [isLoading, setIsLoading] = useState(false)
-	const [error, setError] = useState('')
 
 	const sectionStyles = className(styles.section, styles.center)
 
-	async function onSubmit(event: Event, date: string) {
-		setError(() => '')
-		setIsLoading(() => true)
-		// Saves form data to sessionStorage to restore them after submiting failure
-		setSessionStorage({ formData: event })
+	async function onSubmit(data: RefuelFormData) {
+		setIsLoading(true)
+
 		const response = await fetch('/api/user/refuel', {
 			method: 'POST',
-			body: JSON.stringify({
-				...event,
-				date
-			})
+			body: JSON.stringify(data)
 		})
 
 		if (!response.ok) {
 			const error: Status = await response.json()
 
-			setError(() => error.code)
+			addNotification({ type: 'error', content: getMessage(error.code).text })
 			setIsLoading(() => false)
 			return
 		}
 
-		getEvents()
-			.then(() => {
-				setSessionStorage({ formData: undefined })
-				router.replace('/dashboard')
+		try {
+			await getEvents()
+			router.replace('/dashboard')
+		} catch (error: any) {
+			addNotification({
+				type: 'error',
+				content: getMessage(error.code).text
 			})
-			.catch((error) => {
-				setError(() => error)
-			})
-			.finally(() => {
-				setIsLoading(() => false)
-			})
-	}
-
-	if (isLoading) {
-		return <LoadingIcon type="car" />
+		} finally {
+			addNotification({ type: 'success', content: getMessage('event-added').text })
+			setIsLoading(() => false)
+		}
 	}
 
 	return (
 		<>
 			<Head title="Fuely | Refuel" description="Fuely refuel form page" />
-			<Section title="Refuel" className={sectionStyles}>
+			<Section title="Refuel" className={sectionStyles} disableContent={isLoading}>
 				<RefuelForm onSubmit={onSubmit} />
-				<Error show={error !== ''} text={errorTypes[error]} />
 			</Section>
+			{isLoading && <LoadingIcon type="car" center />}
 		</>
 	)
 }
