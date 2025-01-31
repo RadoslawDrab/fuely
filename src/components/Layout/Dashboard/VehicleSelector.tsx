7f522f49399147335952a76c4298402a72a62b70
RadoslawDrab <radoslaw.drab03@gmail.com>
2025-01-31 15:09:02 +0100

    added props

--- src/components/Layout/Dashboard/VehicleSelector.tsx
+++ src/components/Layout/Dashboard/VehicleSelector.tsx
@@ -1,26 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { className } from '@/utils'
 import useAppContext from '@/hooks/Other/use-app-context.ts'
 
+import { Props } from '@comp/Layout/Dashboard/types/VehicleSelector.modal.ts'
+
 import styles from '@styles/pages/Dashboard/index.module.scss'
 import generalStyles from '@styles/styles.module.scss'
 
-export default function VehicleSelector() {
+export default function VehicleSelector(props: Props) {
     const vehicle = useAppContext().Vehicle
     const { getText } = useAppContext().Language
+    const [currentValue, setCurrentValue] = useState<string>(vehicle.currentVehicle?.id ?? 'null')
 
     useEffect(() => {
-        if (vehicle.vehicles.length == 0) return
-        vehicle.changeVehicle(vehicle.currentVehicle?.id ?? vehicle.vehicles[0].id)
-    }, [vehicle.vehicles, vehicle.currentVehicle, vehicle]);
+        if (vehicle.vehicles.length == 0 || props.chooseOnly) return
+        vehicle.changeVehicle(vehicle.currentVehicle?.id ?? null)
+    }, [vehicle.vehicles, vehicle.currentVehicle, vehicle, props.chooseOnly]);
     
     if (!vehicle.hasVehicles) return <span className={ className(styles.selector, 'text-center') }>{getText('Add vehicle first')}</span>
 
     function onInput(event: React.ChangeEvent<HTMLSelectElement>) {
-        vehicle.changeVehicle(event.target.value)
+        const value = event.target.value.match('null') ? null : event.target.value
+        !props.chooseOnly && vehicle.changeVehicle(value)
+        props.onInput && props.onInput(value)
+        setCurrentValue(event.target.value)
     }
-    return <select className={ className(generalStyles.select, styles.selector, 'fs-600') } onInput={onInput} value={vehicle.currentVehicle?.id}>
+    return <select className={ className(generalStyles.select, styles.selector, 'fs-600') } onInput={onInput} value={currentValue}>
+        {
+            props.allowNull ? <option value='null'>{getText('Unassigned')}</option> : <></>
+        }
         {
             vehicle.vehicles.map((vehicle) => {
                 return <option key={vehicle.id} value={vehicle.id}>
