9f1af8dbd98e3d1c7644256973e2bd565f5a1c9e
RadoslawDrab <radoslaw.drab03@gmail.com>
2025-02-03 14:15:04 +0100

    added VehicleItem component

---
+++ src/components/pages/Vehicles/types/VehicleItem.modal.ts
@@ -0,0 +1,11 @@
+import { Vehicle } from '@api/data/types/index.modal.ts'
+import React from 'react'
+
+export interface Props {
+    vehicle: Vehicle
+    index: number
+    editedVehicleId: string | null
+    onEdit: (event: React.MouseEvent<HTMLButtonElement>) => void
+    onRemove: (event: React.MouseEvent<HTMLButtonElement>) => void
+    onValueChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
