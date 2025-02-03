f63335f6dd3a26ca74eb048626fd34a04d7c4cec
RadoslawDrab <radoslaw.drab03@gmail.com>
2025-02-03 17:15:13 +0100

    modified select component

--- src/components/UI/types/Select.modal.ts
+++ src/components/UI/types/Select.modal.ts
@@ -2,9 +2,13 @@ export interface SelectProps {
 	id?: string
 	name?: string
 	className?: string
-	options: { name?: any; value: string; selected?: boolean }[]
+	wrapperClassName?: string
+	options: (Option | null)[]
 	getValue?: (value: string) => void
 	onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void
 	onFocus?: (event: React.FocusEvent<HTMLSelectElement>) => void
 	onBlur?: (event: React.FocusEvent<HTMLSelectElement>) => void
+	data?: { [key: string]: number | string | boolean | object }
+	useDefaultValue?: boolean
 }
+export type Option = { name?: any; value: string; selected?: boolean, className?: string }
